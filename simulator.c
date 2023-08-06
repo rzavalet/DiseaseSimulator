a8f70f743be58508e09d57b32c7048ec013825c4
Ricardo Zavaleta <rj.zavaleta@gmail.com>
2023-08-06 14:52:51 -0600

    Completed simulator

--- simulator.c
+++ simulator.c
@@ -12,41 +12,75 @@
 #include <raylib.h>
 #include <math.h>
 #include <assert.h>
+#include <string.h>
 
-#include "location.h"
 
-#define WIDTH   900
-#define HEIGHT  600
+/* Disease dynamics */
+#define POPULATION_SIZE         (1000)
+#define DAY_LENGTH              (24)
+#define INFECTION_DURATION      (15 * DAY_LENGTH)
+#define INFECTION_PROBABILITY   (1)
+#define NORMAL_FATALITY_RATE    (10)
+#define SATURATED_FATALITY_RATE (50)
+#define INFECTION_PROXIMITY     (10.0f)
+#define SATURATION_THRESHOLD    (POPULATION_SIZE / 5)
 
-#define POPULATION_SIZE     500
-#define THINK_TIME          1
+
+/* Health system policies */
+#define SOCIAL_DISTANCING (0x01)
+#define ISOLATION         (0x02)
+
+
+/* Display settings */
+#define SCALE             100
+#define WIDTH             (16 * SCALE)
+#define HEIGHT            (9 * SCALE)
+#define SIMULATION_WIDTH  (WIDTH/2)
+#define SIMULATION_HEIGHT (HEIGHT)
+#define GRAPH_WIDTH       (WIDTH - SIMULATION_WIDTH)
+#define GRAPH_HEIGHT      (HEIGHT)
+#define FONT_SIZE         (30)
+#define LINE_SPACING      (10)
+#define COLUMN_SPACING    (20)
+
+
+
+#define CONSTRAINT(x, min, max)   (x = (x < min ? min : (x > max ? max : x)))
 
 #define FOR_STATUS_LIST(DO)   \
-  DO(0, VULNERABLE)  \
-  DO(1, INFECTED)    \
-  DO(2, IMMUNE)      \
-  DO(3, DEAD)
+  DO(0, VULNERABLE, WHITE)    \
+  DO(1, INFECTED, RED)        \
+  DO(2, IMMUNE, GREEN)        \
+  DO(3, DEAD, BLACK)
 
 typedef enum
 {
-#define STATUS_ITEMS(id, name)  name = id,
+#define STATUS_ITEMS(id, name, color) name = id,
   FOR_STATUS_LIST(STATUS_ITEMS)
 #undef STATUS_ITEMS
   DISEASE_STATUS_COUNT
 } disease_status;
 
 const char *status_label[DISEASE_STATUS_COUNT] = {
-#define STATUS_ITEMS(id, name)  #name,
+#define STATUS_ITEMS(id, name, color)  #name,
   FOR_STATUS_LIST(STATUS_ITEMS)
 #undef STATUS_ITEMS
 };
 
-disease_status disease_table[DISEASE_STATUS_COUNT] =
-{ 
-#define STATUS_ITEMS(id, name)  name,
+Color color_map[DISEASE_STATUS_COUNT] =
+{
+#define STATUS_ITEMS(id, name, color)  color,
   FOR_STATUS_LIST(STATUS_ITEMS)
 #undef STATUS_ITEMS
 };
+
+
+typedef struct
+{
+  int x;
+  int y;
+
+} location_t;
 
 typedef struct
 {
@@ -55,154 +89,213 @@ typedef struct
   int             remaining_infection_days;
 } person_t;
 
-#define INFECTION_DURATION    (200)
-#define INFECTION_PROBABILITY (0.5f)
+typedef struct 
+{
+  unsigned int *infected; 
+  size_t  count;
+  size_t  capacity;
+} disease_history_t;
 
-/* Disease dynamics */
-#define NORMAL_FATALITY_RATE    (10)
-#define INFECTION_PROXIMITY     (50.0)
 
-#define SATURATION_THRESHOLD  (POPULATION_SIZE / 5)
 
+unsigned int counters[DISEASE_STATUS_COUNT] = {0};
 person_t  people[POPULATION_SIZE];
-int num_susceptible = 0;
+disease_history_t history = {0};
+bool paused = false;
+bool system_saturated = false;
+unsigned int health_system_policy = 0;
+
+
+
+void history_append(unsigned int e, disease_history_t *history)
+{
+  if (history->count >= history->capacity) {
+    history->capacity = (history->capacity <= 0) ? 1024 : history->capacity << 1;
+
+    history->infected = realloc(history->infected, history->capacity * sizeof(*history->infected));
+    assert(history->infected);
+  }
+
+  history->infected[history->count++] = e;
+}
 
 location_t random_location(void)
 {
   location_t location;
 
-  location.x = rand() % WIDTH;
-  location.y = rand() % HEIGHT;
+  location.x = rand() % SIMULATION_WIDTH;
+  location.y = rand() % SIMULATION_HEIGHT;
 
   return location;
 }
 
 void init_people(void)
 {
-  for (size_t i = 0; i < sizeof(people)/sizeof(people[0]); i++) {
+  for (size_t i = 0; i < POPULATION_SIZE; i++) {
     people[i].current_location = random_location();
-    disease_status tmp = rand() % 100 < 5 ? INFECTED : VULNERABLE;
-    people[i].status = tmp;
+    people[i].status = rand() % 100 < 5 ? INFECTED : VULNERABLE;
     people[i].remaining_infection_days = INFECTION_DURATION;
-    if (people[i].status == VULNERABLE || people[i].status == INFECTED) num_susceptible ++;
+    counters[people[i].status] ++;
   }
 }
 
-void print_people_status(void)
+void draw_people()
 {
-  for (size_t i = 0; i < sizeof(people)/sizeof(people[0]); i++) {
-    printf("(%d, %d) - %s\n", 
-      people[i].current_location.x,
-      people[i].current_location.y,
-      status_label[people[i].status]);
-  }
-}
-
-void draw_people(void)
-{
-  for (size_t i = 0; i < sizeof(people)/sizeof(people[0]); i++) {
-    Color color;
-    switch(people[i].status) {
-      case VULNERABLE:
-        color = WHITE;
-        break;
-      case INFECTED:
-        color = RED;
-        break;
-      case IMMUNE:
-        color = GREEN;
-        break;
-      case DEAD:
-        color = BLACK;
-        break;
-      default:
-        assert("Invalid status" == NULL);
-    }
-
-
+  for (size_t i = 0; i < POPULATION_SIZE; i++) {
     DrawRectangle(people[i].current_location.x, 
                   people[i].current_location.y, 
                   5,
                   5,
-                  color);
+                  color_map[people[i].status]);
   }
 }
+
+void plot_graph(disease_history_t *history)
+{
+  char msg[128];
+  int text_size;
+
+  float x1, y1, x2, y2;
+  Color color;
+  float max_y = history->infected[0];
+
+  for (size_t i = 1; i < history->count; i++)
+    if (history->infected[i] > max_y) 
+      max_y = history->infected[i]; 
+
+  for (size_t i = 0; i < history->count - 1; i++) {
+
+    x1 = 5 + SIMULATION_WIDTH + i * (GRAPH_WIDTH / (float) history->count);
+    y1 = GRAPH_HEIGHT - history->infected[i] * (GRAPH_HEIGHT / max_y);
+
+    x2 = 5 + SIMULATION_WIDTH + (i+1) * (GRAPH_WIDTH / (float) history->count);
+    y2 = GRAPH_HEIGHT - history->infected[i+1] * (GRAPH_HEIGHT / max_y);
+
+    color = history->infected[i] > SATURATION_THRESHOLD ? RED : GREEN;
+    DrawLineEx((Vector2){x1, y1}, (Vector2){x2, y2}, 2.0, color);
+
+  }
+
+  snprintf(msg, sizeof(msg), "Days elapsed: %lu", history->count / DAY_LENGTH);
+  text_size = MeasureText(msg, FONT_SIZE);
+  DrawText(msg, WIDTH - text_size - COLUMN_SPACING, LINE_SPACING + FONT_SIZE, FONT_SIZE, WHITE);
+
+  snprintf(msg, sizeof(msg), "Infected: %d / %d", counters[INFECTED], POPULATION_SIZE);
+  text_size = MeasureText(msg, FONT_SIZE);
+  DrawText(msg, WIDTH - text_size - COLUMN_SPACING, 2 * (LINE_SPACING + FONT_SIZE), FONT_SIZE, RED);
+
+  snprintf(msg, sizeof(msg), "Dead: %d / %d", counters[DEAD], POPULATION_SIZE);
+  text_size = MeasureText(msg, FONT_SIZE);
+  DrawText(msg, WIDTH - text_size - COLUMN_SPACING, 3 * (LINE_SPACING + FONT_SIZE), FONT_SIZE, BLACK);
+}
+
 
 double distance(location_t a, location_t b)
 {
   return sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
 }
 
-#define CONSTRAINT(x, min, max)   (x = (x < min ? min : (x > max ? max : x)))
-
-void simulation_step()
+void move_people(void)
 {
-  if (num_susceptible == 0) {
-    const char *msg = "Simulation has finished";
-    int text_size = MeasureText(msg, 30);
-    DrawText(msg, WIDTH/2 - text_size/2, HEIGHT/2 - 30/2, 30, RED);
-    return;
+  memset(counters, 0, sizeof(counters));
+
+  for (size_t i = 0; i < POPULATION_SIZE; i++) {
+
+    counters[people[i].status] ++;
+
+    if (people[i].status == DEAD) continue;
+
+    if ((health_system_policy & ISOLATION) && people[i].status == INFECTED) continue;
+
+    people[i].current_location.x += (int)(rand() % 11) - 5;
+    CONSTRAINT(people[i].current_location.x, 0, SIMULATION_WIDTH);
+
+    people[i].current_location.y += (int)(rand() % 11) - 5;
+    CONSTRAINT(people[i].current_location.y, 0, SIMULATION_HEIGHT);
   }
 
-  for (size_t i = 0; i < sizeof(people)/sizeof(people[0]) - 1; i++) {
-    for (size_t j = i + 1; j < sizeof(people)/sizeof(people[0]); j++) {
+  history_append(counters[INFECTED], &history);
+}
+
+void kill_or_recover(person_t *target)
+{
+  if (target->status != INFECTED) return;
+
+  if (-- target->remaining_infection_days <= 0)
+    target->status = (rand() % 100 <= (system_saturated ? SATURATED_FATALITY_RATE : NORMAL_FATALITY_RATE)) ? DEAD : IMMUNE;
+}
+
+void infect_people(person_t *a, person_t *b)
+{
+  if (a->status == INFECTED && b->status == VULNERABLE && (rand() % 100 <= INFECTION_PROBABILITY)) {
+    b->status = INFECTED;
+    b->remaining_infection_days = INFECTION_DURATION;
+  }
+  else if (b->status == INFECTED && a->status == VULNERABLE && (rand() % 100 <= INFECTION_PROBABILITY)) {
+    a->status = INFECTED;
+    a->remaining_infection_days = INFECTION_DURATION;
+  }
+}
+
+void scan_people(void)
+{
+  for (size_t i = 0; i < POPULATION_SIZE - 1; i++) {
+    for (size_t j = i + 1; j < POPULATION_SIZE; j++) {
 
       double d = distance(people[i].current_location, people[j].current_location);
 
       if (d <= INFECTION_PROXIMITY) {
-        if (people[i].status == INFECTED) {
-          if (-- people[i].remaining_infection_days <= 0) {
-            if (rand() % 100 <= NORMAL_FATALITY_RATE) people[i].status = DEAD;
-            else people[i].status = IMMUNE;
-          }
-        }
-        if (people[j].status == INFECTED) {
-          if (-- people[j].remaining_infection_days <= 0) {
-            if (rand() % 100 <= NORMAL_FATALITY_RATE) people[j].status = DEAD;
-            else people[j].status = IMMUNE;
-          }
-        }
 
-        if (people[i].status == INFECTED && people[j].status == VULNERABLE) {
-          people[j].status = INFECTED;
-          people[j].remaining_infection_days = INFECTION_DURATION;
-        }
-        else if (people[j].status == INFECTED && people[i].status == VULNERABLE) {
-          people[i].status = INFECTED;
-          people[i].remaining_infection_days = INFECTION_DURATION;
-        }
+        kill_or_recover(&people[i]);
+        kill_or_recover(&people[j]);
 
+        infect_people(&people[i], &people[j]);
       }
     }
   }
 
-  num_susceptible = 0;
-  for (size_t i = 0; i < sizeof(people)/sizeof(people[0]); i++) {
-    if (people[i].status == DEAD) continue;
-    if (people[i].status == VULNERABLE || people[i].status == INFECTED) num_susceptible ++;
+}
 
-    people[i].current_location.x += (int)(rand() % 11) - 5;
-    CONSTRAINT(people[i].current_location.x, 0, WIDTH);
-
-    people[i].current_location.y += (int)(rand() % 11) - 5;
-    CONSTRAINT(people[i].current_location.y, 0, HEIGHT);
+void simulation_step()
+{
+  if (counters[INFECTED] == 0) {
+    const char *msg = "Simulation has finished";
+    int text_size = MeasureText(msg, FONT_SIZE);
+    DrawText(msg, WIDTH/2 - text_size/2, HEIGHT/2 - FONT_SIZE/2, FONT_SIZE, RED);
+    return;
   }
+
+  scan_people();
+  move_people();
+
+  system_saturated = (counters[INFECTED] >= SATURATION_THRESHOLD) ? true : false;
 }
 
 int main(void)
 {
   srand(time(NULL));
   init_people();
-  //print_people_status();
+  history_append(0, &history);
 
   InitWindow(WIDTH, HEIGHT, "Covid simulation");
-  SetTargetFPS(60);                                 // Set target FPS (maximum)
+  SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
+    if (IsKeyPressed(KEY_SPACE)) {
+      paused = !paused;
+    }
+
+    if (IsKeyPressed(KEY_ENTER)) {
+      init_people();
+      history.count = 0;
+      history_append(0, &history);
+    }
+
     BeginDrawing();
       ClearBackground(BLUE);
+      if (!paused) simulation_step();
       draw_people();
-      simulation_step();
+      plot_graph(&history);
     EndDrawing();
   }
 
